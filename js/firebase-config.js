b220e6422fdfebb0969e56f767b09e210697f6cc
ros-admin <helpline.ros@gmail.com>
2026-06-17 07:53:16 +0600

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8NSt3VP9jnVkzrGKPvNSqbnm7hwL-NqY",
+  authDomain: "rajshahi-olimpiad-society.firebaseapp.com",
+  projectId: "rajshahi-olimpiad-society",
+  storageBucket: "rajshahi-olimpiad-society.firebasestorage.app",
+  messagingSenderId: "248298850450",
+  appId: "1:248298850450:web:d9e99750eddd9c7b18be4b"
+};
+
+export const app = initializeApp(firebaseConfig);
